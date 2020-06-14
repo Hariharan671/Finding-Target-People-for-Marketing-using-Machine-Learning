7302a527e95cc586af91ec630a1bbf1f67a1b554
Hariharan671 <haran8861@gmail.com>
2020-06-14 19:24:03 +0530

    initial commit

---
+++ Heirachical_complete_linkage.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('mall.csv')
+X = dataset.iloc[:,[3,4]].values
+
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X,method='complete'))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distances')
+plt.show()
+
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters=5,affinity='euclidean',linkage='single')
+y_hc = hc.fit_predict(X)
+
+plt.scatter(X[y_hc==0,0],X[y_hc==0,1],s=100,c='red',label='Careful')
+plt.scatter(X[y_hc==1,0],X[y_hc==1,1],s=100,c='blue',label='Standard')
+plt.scatter(X[y_hc==2,0],X[y_hc==2,1],s=100,c='green',label='Target')
+plt.scatter(X[y_hc==3,0],X[y_hc==3,1],s=100,c='cyan',label='Careless')
+plt.scatter(X[y_hc==4,0],X[y_hc==4,1],s=100,c='magenta',label='Sensible')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income(k$)')
+plt.ylabel('Spending Score(1-100)')
+plt.legend()
+plt.show()
